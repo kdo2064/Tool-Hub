ee065170f29a4de93d1cf929c5c6ab4ad322416d
KDO <114388910+kdo2064@users.noreply.github.com>
2023-03-13 16:23:57 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import time
+from fun.mainfunction import *
+
+from fun.logo import *
+
+if __name__=="__main__":
+    os.system("clear")
+    print(logo)
+    print(mainmenu)
+    mainoption()
---
+++ tools.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+def zphisher():
+    os.system("pkg install tur-repo && ")
+
+def camphish():
+    os.system("cd $HOME && git clone https://github.com/techchipnet/CamPhish && cd CamPhish && bash camphish.sh")
+
+def tigervirus():
+    os.system("cd $HOME && apt update && apt upgrade -y &&  && git clone https://github.com/Devil-Tigers/TigerVirus.git && cd TigerVirus && bash TigerVirus.sh")
+
+def THYDRA():
+    os.system("cd $HOME && apt update && apt upgrade && git clone https://github.com/isuruwa/T-HYDRA && cd T-HYDRA/Files && bash thyins.sh")
+
+def BruteX():
+    os.system("cd $HOME && apt update && apt upgrade && apt install git -y && git clone https://github.com/MrHacker-X/BruteX.git && cd BruteX && chmod +x * && bash setup.sh")
+
+def ighack():
+    os.system("cd $HOME && apt-get update -y && apt-get upgrade -y && pkg install python -y && pkg install python2 -y && pkg install git -y && pip install lolcat && git clone https://github.com/noob-hackers/ighack && cd $HOME && cd ighack && bash setup")
+
+def SocialBox():
+    os.system("cd $HOME && apt-get update && apt-get install git && git clone https://github.com/samsesh/SocialBox-Termux.git && cd SocialBox-Termux && chmod +x install-sb.sh && ./install-sb.sh")
+
+def maskphish():
+    os.system("cd $HOME && git clone https://github.com/jaykali/maskphish && cd maskphish && bash maskphish.sh")
+
+
+
+
