50ed7f3e97859983f54447e489d5dcf2a579bb44
KDO <114388910+kdo2064@users.noreply.github.com>
2023-03-14 18:27:29 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import time
+import os
+import time
+from fun.mainfunction import *
+import requests
+from fun.logo import *
+
+
+black="\033[0;30m"
+red="\033[0;31m"
+bred="\033[1;31m"
+green="\033[0;32m"
+bgreen="\033[1;32m"
+yellow="\033[0;33m"
+byellow="\033[1;33m"
+blue="\033[0;34m"
+bblue="\033[1;34m"
+purple="\033[0;35m"
+bpurple="\033[1;35m"
+cyan="\033[0;36m"
+bcyan="\033[1;36m"
+white="\033[0;37m"
+nc="\033[00m"
+
+
+
+
+if __name__=="__main__":   
+        os.system("clear")
+        print(logo)
+        os.system("pkg install play-audio $$ pkg install sox -y $$ pip install requests && rm -rf setup.py && python3 main.py")
+
+
+
